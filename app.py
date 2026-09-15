be30907258bb07ec1d00731adf7c091bd6b9598c
Caio Samuel <caio.samuel.c.n@gmail.com>
2026-09-15 20:11:00 -0300

    inclusão professor e turma

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask, render_template
 import sqlite3
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/sobre')
 def sobre():
@@ -18,24 +15,24 @@ def sobre():
 def contato():
     return render_template('contato.html')
 
-
 @app.route('/aluno')
 def listar_aluno():
-    #Conectar ao banco de dados SQLite
+    # Conectar ao banco de dados SQLite
     DB_PATH = "banco_escola.db"
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
+    
     # Executar consulta SQL
     cursor.execute("SELECT id, nome, idade, cidade FROM aluno")
+    
     # Obtém todos os registros
     lista = cursor.fetchall()
+    
     # Fecha a conexão com o banco de dados
     conn.close()
-   return render_tamplate("aluno/lista.html", lista=lista)
-
-
-
+    
+    # Indentação e nome da função corrigidos
+    return render_template("aluno/lista.html", lista=lista)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
