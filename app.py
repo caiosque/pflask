0f6bd7bb1f1fe350cbaedecb91b18fa91479d9e8
Caio Samuel <caio.samuel.c.n@gmail.com>
2026-09-16 11:03:13 -0300

    01

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-import sqlite3
+import sqlite3 
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/sobre')
 def sobre():
@@ -15,24 +18,36 @@ def sobre():
 def contato():
     return render_template('contato.html')
 
+
 @app.route('/aluno')
-def listar_aluno():
-    # Conectar ao banco de dados SQLite
+def lista_aluno():
     DB_PATH = "banco_escola.db"
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    
-    # Executar consulta SQL
     cursor.execute("SELECT id, nome, idade, cidade FROM aluno")
-    
-    # Obtém todos os registros
     lista = cursor.fetchall()
-    
-    # Fecha a conexão com o banco de dados
     conn.close()
-    
-    # Indentação e nome da função corrigidos
-    return render_template("aluno/lista.html", lista=lista)
+    return render_template('aluno/lista.html',lista = lista)
+
+@app.route('/professor')
+def lista_professor():
+    DB_PATH = "banco_escola.db"
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, nome, disciplina FROM   professor")
+    lista = cursor.fetchall()
+    conn.close()
+    return render_template('professor/lista.html',lista = lista)
+
+@app.route('/turma')
+def lista_turma():
+    DB_PATH = "banco_escola.db"
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT turma.id, semestre, nome_curso, professor.nome FROM turma join curso on curso.id = turma.curso_id join professor on professor.id=turma.professor_id")
+    lista = cursor.fetchall()
+    return render_template('turma/lista.html',lista = lista)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
